6c08ecd4d3ce5a5adc68948ba4a8f63f36d6247e
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-09-28 19:59:13 -0500

    created my server and learning python

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#yup
+from fastapi import Body, FastAPI
+
+server = FastAPI()
+
+@server.get('/')
+async def welcomeMessage():
+    return {"message": "Hello World"}
+
+@server.get('/something')
+def randomMessage():
+    return {'message': 'Git gud m8'}
+
+@server.post('/something')
+def hateMail(payload: dict = Body(...)):
+    print(payload)
+    return payload
